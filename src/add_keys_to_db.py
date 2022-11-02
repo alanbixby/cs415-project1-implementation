1c9488893cbdbe4bcc446387034d949fae544fd7
Alan Bixby <alanrbixby@gmail.com>
2022-11-02 02:55:28 +0000

    final: refactor odds_api, ready for migration

---
+++ src/add_keys_to_db.py
@@ -0,0 +1,22 @@
+import asyncio
+import os
+import motor.motor_asyncio
+from typing import Dict
+
+# (<email_identifier>, <api_key>)
+keys: Dict[str, str] = {"danny": "01457544e642ecfbe2aa8ec12478e803"}  # expired
+
+
+async def add_keys_to_db() -> None:
+    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+    db = client[os.getenv("DB_NAME", "cs415_data_science_dev")]
+    key_collection = db["odds_api_keys"]
+
+    for _id, api_key in keys.items():
+        await key_collection.find_one_and_update(
+            {"_id": _id},
+            {"$set": {"api_key": api_key, "active": True, "remaining": 500}},
+        )
+
+
+asyncio.run(add_keys_to_db())
